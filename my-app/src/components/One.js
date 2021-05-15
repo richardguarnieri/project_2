bbd4d2631fb0668ace7084ebc9bb3a501d1641c1
Richard Guarnieri <richard.gm@outlook.com>
2021-05-15 12:39:47 -0500

    additional fixes

--- my-app/src/components/One.js
+++ my-app/src/components/One.js
@@ -1,6 +1,36 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+
+
 
 const One = () => {
+
+    const [year, setYear] = useState('');
+    const [genre, setGenre] = useState('');
+    const [language, setLanguage] = useState('');
+
+    // const general = async () => {
+    //     const response = await fetch(`${$API}/all_movies/xxxxx`);
+    //     const data = await response.json();
+    //     console.log(data);
+    // };
+
+    const hello = () => {
+        console.log('hello world')
+    }
+
+    // const selected = async () => {
+    //     const response = await fetch(`${$API}/all_movies/${year}/${genre}/${language}`);
+    //     const data = await response.json();
+    //     console.log(data);
+    // };
+
+    useEffect(() => {
+        console.log('rendered')
+    }, [hello])
+
+
     return (
         <div>
             <div className="container">
@@ -13,6 +43,50 @@ const One = () => {
                 <hr className='mt-4 mb-4'/>
                 <div className="col">
                     <h1 className='text-center'>Chart Area</h1>
+                    <select id='year' className="form-select" aria-label="Default select example" 
+                    onChange={(e) => {
+                      setYear(e.target.value);
+                    }}>
+                        <option selected>Open this select menu</option>
+                        <option value="Year 1">Year 1</option>
+                        <option value="Year 2">Year 2</option>
+                        <option value="Year 3">Year 3</option>
+                    </select>
+                    <select id='genre' className="form-select" aria-label="Default select example"
+                    onChange={(e) => {
+                        setGenre(e.target.value);
+                    }}>
+                        <option selected>Open this select menu</option>
+                        <option value="Genre 1">Genre 1</option>
+                        <option value="Genre 2">Genre 2</option>
+                        <option value="Genre 3">Genre 3</option>
+                    </select>
+                    <select id='language' className="form-select" aria-label="Default select example"
+                    onChange={(e) => {
+                        setLanguage(e.target.value);
+                    }}>
+                        <option selected>Open this select menu</option>
+                        <option value="Language 1">Language 1</option>
+                        <option value="Language 2">Language 2</option>
+                        <option value="Language 3">Language 3</option>
+                    </select>
+
+                    <hr></hr>
+                    
+                    {year}<br></br>
+                    {genre}<br></br>
+                    {language}<br></br>
+
+                    <hr></hr>
+
+
+
+
+                    <h2>CALL D3 CODE</h2>
+
+
+
+
                 </div>
                 <div className="d-flex justify-content-center pt-4">
                         <Link to="/visualizations" className="btn btn-dark btn-lg m-2">Back to Visualizations</Link>
