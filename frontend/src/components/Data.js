450af33ba2173af7599cbb8c769d1ddc347ad1d9
Pilar <pilar.montes01@gmail.com>
2021-05-22 18:38:42 -0500

    Viz 2 completed

--- frontend/src/components/Data.js
+++ frontend/src/components/Data.js
@@ -1,4 +1,41 @@
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import * as d3 from 'd3';
+
 const Data = () => {
+
+const herokuBackend = 'https://itesm-project2-backend.herokuapp.com'
+let tbody = d3.select("tbody");
+
+const filterData = async () => {
+    try {
+        const response = await fetch(`${herokuBackend}/all_movies`);
+        const data = await response.json();
+        deleteTable();
+        console.log("click");
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+
+
+
+//  function filterData(){
+//     console.log("click");
+    
+//     let movie_data = general;
+//     console.log(movie_data);
+
+
+//  }
+ 
+ function deleteTable(){
+     tbody.selectAll("tr").remove()
+        .selectAll("td").remove();
+ }
+    
+    
     return (
         <div>
             <div class="container">
@@ -7,14 +44,14 @@ const Data = () => {
                     <h4 class="pb-3 mb-0">Search for Movies</h4>
                     <form action="" onSubmit="return false;">
                         <div class="form-floating">
-                            <input type="text" class="form-control" id="date" placeholder="1/11/2011" />
-                            <label for="date">Enter a date between 1/1/2010 and 1/13/2010</label>
+                            <input type="text" class="form-control" id="genre" placeholder="Romance" />
+                            <label for="genre">Romance, Drama, Crime, Fantasy, Comedy, Biography...</label>
                         </div>
                         <div class="form-floating mt-3">
-                            <input type="text" class="form-control" id="state" placeholder="1/11/2012" />
-                            <label for="state">Enter a state: i.e. ca</label>
+                            <input type="text" class="form-control" id="language" placeholder="English" />
+                            <label for="language">English, Spanish, Italian, French, German...</label>
                         </div>
-                        <button type='button' class="btn btn-dark mt-3">Find Movies</button>
+                        <button type='button' onClick={filterData} class="btn btn-dark mt-3">Find Movies</button>
                     </form>
                 </div>
             </div>
@@ -24,13 +61,12 @@ const Data = () => {
                         <table class="table table-hover">
                             <thead class="table-light">
                                 <tr>
-                                    <td>Date</td>
-                                    <td>City</td>
-                                    <td>State</td>
+                                    <td>Movie Title</td>
+                                    <td>Genre</td>
+                                    <td>Language</td>
                                     <td>Country</td>
-                                    <td>Shape</td>
                                     <td>Duration</td>
-                                    <td>Comments</td>
+                                    <td>Rating</td>
                                 </tr>
                             </thead>
                             <tbody>
